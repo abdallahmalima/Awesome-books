2a034998fac86eb87a8c1b64adaed1116fcd4fc8
abdallah <abdallahantony55.aa@gmail.com>
2022-10-24 15:42:00 +0300

    Add javacript to get input from form

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-document.getElementsByTagName('body').style.background = 'red';
+const createBookForm = document.querySelector('.create-book');
+
+function clearForm() {
+  createBookForm.elements.title.value = '';
+  createBookForm.elements.author.value = '';
+}
+
+function addBook(event) {
+  event.preventDefault();
+  const title = createBookForm.elements.title.value;
+  const author = createBookForm.elements.author.value;
+  const book = { title, author };
+  
+  clearForm();
+}
+
+
+
+createBookForm.addEventListener('submit', addBook);
